c92233116eeecdec4f44f164ae0fdea230ff7101
Vasyl <wasilius2@gmail.com>
2020-08-12 21:03:42 +0300

    Initial commit

---
+++ src/main/java/org/example/todolist/Application.java
@@ -0,0 +1,13 @@
+package org.example.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(org.example.todolist.Application.class, args);
+    }
+
+}
---
+++ src/main/resources/templates/greeting.ftl
@@ -0,0 +1,8 @@
+<#import "parts/common.ftl" as c>
+
+<@c.page>
+    <h5>Hello, guest</h5>
+    <div>
+        Its my first web application))
+    </div>
+</@c.page>
